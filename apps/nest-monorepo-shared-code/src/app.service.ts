81f8e48505fb7114d6ebfcc5ecb18e037f29cee0
Mayur Solanki <mayursolanki120@gmail.com>
2024-07-02 18:50:12 +0530

    Fixed

--- apps/nest-monorepo-shared-code/src/app.service.ts
+++ apps/nest-monorepo-shared-code/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World from my-app!';
+    return 'Hello World from nest-monorepo-shared-code!!';
   }
 }
